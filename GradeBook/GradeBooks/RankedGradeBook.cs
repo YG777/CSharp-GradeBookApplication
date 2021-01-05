8b9b293de8bf0bbc9cb4f2713a87ae4733e651a6
YG777 <katyake@gmail.com>
2021-01-05 11:41:15 +0000

    completed module

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+using GradeBook.Enums;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        
+
+        public RankedGradeBook(string name):base(name)
+        {
+            Name = name;
+            Type = GradeBookType.Ranked;
+        }
+
+      
+    }
+}
---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+using GradeBook.Enums;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        
+
+        public StandardGradeBook(string name):base(name)
+        {
+            Name = name;
+            Type = GradeBookType.Standard;
+    
+        }
+
+      
+    }
+}

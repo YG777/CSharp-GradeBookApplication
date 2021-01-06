9e2a175b915883d0650a7fa81116f1f684298f4b
YG777 <katyake@gmail.com>
2021-01-06 14:17:21 +0000

    module

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -12,7 +12,6 @@ namespace GradeBook.GradeBooks
     public class StandardGradeBook : BaseGradeBook
     {
         
-
         public StandardGradeBook(string name):base(name)
         {
             Name = name;

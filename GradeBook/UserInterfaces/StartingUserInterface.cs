d557815f1954b9219e972712fec5dbb26aa8ea2f
YG777 <katyake@gmail.com>
2021-01-06 14:28:05 +0000

    again

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -51,24 +51,18 @@ namespace GradeBook.UserInterfaces
                 return;
             }
             var name = parts[1];
-            var type = parts[2];
+            var type = parts[2].ToLower();
 
             BaseGradeBook gradeBook;
 
             if(type == "standard")
-            {
                 gradeBook = new StandardGradeBook(name);
-               
-            }
-            else if(type == "ranked")
-            {
-                gradeBook = new RankedGradeBook(name);
-            }
+            else if(type == "ranked")         
+                gradeBook = new RankedGradeBook(name);         
             else
             {
-                Console.WriteLine("{0} is not a supported type of gradebook, please try again");
+                Console.WriteLine("{0} is not a supported type of gradebook, please try again", type);
                 return;
-
             }
 
             Console.WriteLine("Created gradebook {0}.", name);

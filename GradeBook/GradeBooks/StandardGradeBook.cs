b49a98c067e287cb07a232a96cb4aa8865a056c3
YG777 <katyake@gmail.com>
2021-01-06 15:20:17 +0000

    task

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -11,14 +11,15 @@ namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook
     {
-        
-        public StandardGradeBook(string name):base(name)
+  
+        public StandardGradeBook(string name, bool isWeighted) :base(name, isWeighted)
+            
         {
             Name = name;
             Type = GradeBookType.Standard;
-    
+            IsWeighted = isWeighted;
         }
 
-      
+
     }
 }
